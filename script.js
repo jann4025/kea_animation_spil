4435efaa60b2fbbcd8cd7ba01d7421696008693a
Jannick Holm <jannick@wonderfestiwall.dk>
2018-10-30 14:11:49 +0100

    Javascript tilføjet til start skærm

---
+++ script.js
@@ -0,0 +1,39 @@
+window.addEventListener("load", sidenVises);
+
+function sidenVises() {
+    console.log("siden vises");
+    showStart();
+}
+
+function showStart() {
+    console.log("show start");
+     document.querySelector("#game_background").classList.add("hide");
+    document.querySelector("#start").classList.remove("hide");
+    document.querySelector("#play").classList.add("pulse");
+    document.querySelector("#play").addEventListener("click", playClickHandler)
+}
+
+
+function playClickHandler() {
+    hideStart();
+}
+
+function hideStart() {
+    console.log("hide start");
+    document.querySelector("#play").removeEventListener("click", playClickHandler);
+    document.querySelector("#play").classList.remove("pulse");
+
+    document.querySelector("#start").classList.add("fade_out");
+    document.querySelector("#start").addEventListener("animationend", startGame)
+
+
+}
+
+function startGame() {
+    console.log("start game");
+     document.querySelector("#start").removeEventListener("animationend", startGame);
+    document.querySelector("#game_background").classList.remove("hide");
+     document.querySelector("#start").classList.add("hide");
+    document.querySelector("#start").classList.remove("fade_out");
+
+}
